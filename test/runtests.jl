8674c91acb508f2346e1019274e0faa2d3c5b5da
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2020-02-21 18:00:37 +0100

    Add tests for all implemented sets.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,57 @@ using Test
 const CP = ConstraintProgrammingExtensions
 
 @testset "JuCP" begin
-    @testset "AllDifferent" begin
-        m = Model()
-        @variable(m, x)
-        @variable(m, y)
-        @variable(m, z)
+    @testset "Sets" begin
+        @testset "AllDifferent" begin
+            m = Model()
+            @variable(m, x)
+            @variable(m, y)
+            @variable(m, z)
 
-        @constraint(m, cref, alldifferent(x, y, z))
+            @constraint(m, cref, alldifferent(x, y, z))
 
-        c = JuMP.constraint_object(cref)
-        @test c.func == [x, y, z]
-        @test c.set == CP.AllDifferent(3)
+            c = JuMP.constraint_object(cref)
+            @test c.func == [x, y, z]
+            @test c.set == CP.AllDifferent(3)
+        end
+
+        @testset "DifferentFrom" begin
+            m = Model()
+            @variable(m, x)
+            @variable(m, y)
+
+            @constraint(m, cref, x != y)
+
+            c = JuMP.constraint_object(cref)
+            @test c.func == x - y
+            @test c.set == CP.DifferentFrom(0.0)
+        end
+
+        @testset "Strictly(LessThan)" begin
+            m = Model()
+            @variable(m, x)
+            @variable(m, y)
+
+            @constraint(m, cref, x < y)
+
+            c = JuMP.constraint_object(cref)
+            @test c.func == x - y
+            @test c.set == CP.Strictly(MOI.LessThan(0.0))
+        end
+
+        @testset "Strictly(GreaterThan)" begin
+            m = Model()
+            @variable(m, x)
+            @variable(m, y)
+
+            @constraint(m, cref, x > y)
+
+            c = JuMP.constraint_object(cref)
+            @test c.func == x - y
+            @test c.set == CP.Strictly(MOI.GreaterThan(0.0))
+        end
+    end
+
+    @testset "Bridges" begin
     end
 end
